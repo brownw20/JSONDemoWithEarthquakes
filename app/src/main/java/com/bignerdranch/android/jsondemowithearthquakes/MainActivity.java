a5aeeda2e83a42be0e665ec656ef530aa4681db2
willyb467 <48896457+willyb467@users.noreply.github.com>
2020-04-28 11:12:07 -0400

    first commit

---
+++ app/src/main/java/com/bignerdranch/android/jsondemowithearthquakes/MainActivity.java
@@ -0,0 +1,83 @@
+package com.bignerdranch.android.jsondemowithearthquakes;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String REQUEST_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02";
+
+    private RecyclerView mRecyclerView;
+    private EarthquakeAdapter mAdapter;
+    private TextView mEmptyStateTextView;
+    private View mProgressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mAdapter = new EarthquakeAdapter(new ArrayList<Earthquake>());
+        mRecyclerView.setAdapter(mAdapter);
+
+        mEmptyStateTextView = findViewById(R.id.empty_view);
+        mProgressBar = findViewById(R.id.loading_spinner);
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if(networkInfo != null && networkInfo.isConnected()){
+            EarthQuakeAsyncTask task = new EarthQuakeAsyncTask();
+            task.execute(REQUEST_URL);
+        } else{
+            mProgressBar.setVisibility(View.GONE);
+            mEmptyStateTextView.setText("No Internet Connection");
+            mEmptyStateTextView.setVisibility(View.VISIBLE);
+        }
+
+        EarthQuakeAsyncTask task = new EarthQuakeAsyncTask();
+        task.execute(REQUEST_URL);
+    }
+    private class EarthQuakeAsyncTask extends AsyncTask<String, Void, List<Earthquake>>{
+        @Override
+        public List<Earthquake> doInBackground(String... urls){
+            if(urls != null && urls.length > 0){
+                List<Earthquake> results = NetworkingUtilities.fetchEarthQuakeData(urls[0]);
+                return results;
+            }
+            return null;
+        }
+        @Override
+        public void onPostExecute(List<Earthquake> earthquakes){
+            mProgressBar.setVisibility(View.GONE);
+            if(earthquakes != null && earthquakes.size()>0){
+                mEmptyStateTextView.setVisibility(View.GONE);
+                mRecyclerView.setVisibility(View.VISIBLE);
+                mAdapter.setData(earthquakes);
+                mAdapter.notifyDataSetChanged();
+            } else{
+                mRecyclerView.setVisibility(View.GONE);
+                mEmptyStateTextView.setText("No earthquakes found");
+                mEmptyStateTextView.setVisibility(View.VISIBLE);
+                //hello
+            }
+        }
+    }
+}
+
+
---
+++ app/src/main/java/com/bignerdranch/android/jsondemowithearthquakes/NetworkingUtilities.java
@@ -0,0 +1,105 @@
+package com.bignerdranch.android.jsondemowithearthquakes;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public final class NetworkingUtilities {
+    public static final String LOG_TAG = "NetworkingUtilities";
+    /** Sample JSON response for a USGS query */
+    private NetworkingUtilities() {
+    }
+
+     // Returns a list of Earthquake objects that has been built up from parsing a JSON response.
+    public static ArrayList<Earthquake> convertJSONtoEarthquakes(String jsonResponse) {
+        ArrayList<Earthquake> earthquakes = new ArrayList<>();
+        try {
+            JSONObject baseJSONResponse = new JSONObject(jsonResponse);
+            JSONArray earthquakeArray = baseJSONResponse.getJSONArray("features");
+            for (int i = 0; i < earthquakeArray.length(); i++) {
+                //hello
+                JSONObject currentEarthquake = earthquakeArray.getJSONObject(i);
+                JSONObject properties = currentEarthquake.getJSONObject("properties");
+                double magnitude = properties.getDouble("mag");
+                String location = properties.getString("place");
+                long time = properties.getLong("time");
+                String url = properties.getString("url");
+                Earthquake newEarthquake = new Earthquake(magnitude, location, time, url);
+                earthquakes.add(newEarthquake);
+            }
+        } catch (JSONException e) {
+            Log.e("QueryUtils", "Problem parsing the earthquake JSON results", e);
+        }
+
+        return earthquakes;
+    }
+    public static List<Earthquake> fetchEarthQuakeData(String requestUrlString){
+        List<Earthquake> results = null;
+        try{
+            URL requestUrl = new URL(requestUrlString);
+            String response = makeHttpRequest(requestUrl);
+            results = convertJSONtoEarthquakes(response);
+        }
+        catch(Exception e){
+            Log.e(LOG_TAG,"Error with creating URL",e);
+        }
+        return results;
+    }
+    private static String makeHttpRequest(URL url) throws IOException{
+        HttpsURLConnection urlConnection = null;
+        String jsonResponse = "";
+        InputStream inputStream = null;
+        try{
+            urlConnection = (HttpsURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setConnectTimeout(15000);
+            urlConnection.setReadTimeout(10000);
+            urlConnection.connect();
+            int response = urlConnection.getResponseCode();
+            if(response == HttpsURLConnection.HTTP_OK) {
+                inputStream = urlConnection.getInputStream();
+                if (inputStream != null) {
+                    jsonResponse = readFromStream(inputStream);
+                }
+            }
+            else{
+                Log.e(LOG_TAG,"Error response code: "+response);
+            }
+        } catch(IOException e){
+            Log.e(LOG_TAG,"Problem retrieving the earthquake JSON results",e);
+        } finally{
+            if(urlConnection != null){
+                urlConnection.disconnect();
+            }
+            if(inputStream != null ){
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
+    private static String readFromStream(InputStream inputStream) throws IOException{
+        StringBuilder output = new StringBuilder();
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream,"UTF-8");
+        BufferedReader reader = new BufferedReader(inputStreamReader);
+        String line = reader.readLine();
+        while(line!=null){
+            output.append(line);
+            line = reader.readLine();
+        }
+        return output.toString();
+    }
+
+}
+
